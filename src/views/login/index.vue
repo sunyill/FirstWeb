b0c40a93b2fb325233e810ab7502dcfcead1bacd
sunyill <sunyill0707@163.com>
2019-09-26 18:04:44 +0800

    4.登录页面,页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,139 @@
  * @Description: 登录页
  * @Author: your name
  * @Date: 2019-09-26 17:14:16
- * @LastEditTime: 2019-09-26 17:15:22
+ * @LastEditTime: 2019-09-26 18:03:39
  * @LastEditors: Please set LastEditors
  -->
 <template>
-    <div>this is 登录页</div>
+  <div class="login">
+    <el-card class="box-card">
+      <div class="login_title_icon">
+        <img src="../../assets/image/logo_index.png" alt="首页图标" />
+      </div>
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        label-width="60px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="手机号" prop="pass">
+          <el-input type="password" v-model="ruleForm.pass" autocomplete="off" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item label="验证码" prop="checkPass" >
+          <el-input
+            type="password"
+            v-model="ruleForm.checkPass"
+            autocomplete="off"
+            placeholder="请输入验证码"
+            style="width:60%"
+          ></el-input>
+          <el-button type="primary" style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox style="float:left" name="type">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="passlogin">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
-
 <script>
 export default {
-
+  data () {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('年龄不能为空'))
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error('请输入数字值'))
+        } else {
+          if (value < 18) {
+            callback(new Error('必须年满18岁'))
+          } else {
+            callback()
+          }
+        }
+      }, 1000)
+    }
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.ruleForm.checkPass !== '') {
+          this.$refs.ruleForm.validateField('checkPass')
+        }
+        callback()
+      }
+    }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      ruleForm: {
+        pass: '',
+        checkPass: '',
+        age: ''
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: 'blur' }],
+        checkPass: [{ validator: validatePass2, trigger: 'blur' }],
+        age: [{ validator: checkAge, trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login {
+  width: 100%;
+  height: 100vh;
+  background: url("../../assets/image/mylogin.jpg");
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box-card {
+    width: 420px;
+    height: 330px;
+    .login_title_icon {
+      width: 200px;
+      height: 40px;
+      margin: 10px auto;
+      img {
+        width: 100%;
+      }
+    }
+    .passlogin {
+      margin-left: -30px;
+      width: 100%;
+    }
+  }
+}
 </style>
