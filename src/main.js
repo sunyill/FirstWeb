203ee2c70bfad5745892930fddd9b9dbb6fed490
sunyill <sunyill0707@163.com>
2019-09-26 17:03:48 +0800

    2.引入elementUi

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
+/*
+ * @Description: 全局的入口
+ * @Author: your name
+ * @Date: 2019-09-25 22:40:24
+ * @LastEditTime: 2019-09-26 17:03:07
+ * @LastEditors: Please set LastEditors
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 导入element-Ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,13 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-25 22:40:24
- * @LastEditTime: 2019-09-26 16:36:05
+ * @LastEditTime: 2019-09-26 17:03:18
  * @LastEditors: Please set LastEditors
  -->
 <template>
   <div class="home">
     this is home page
+
   </div>
 </template>
 
@@ -15,9 +16,6 @@
 // @ is an alias to /src
 
 export default {
-
-  components: {
-
-  }
+  components: {}
 }
 </script>
