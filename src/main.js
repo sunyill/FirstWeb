e2163f5c697c66c5bb06447c159f964feda4ce2c
sunyill <sunyill0707@163.com>
2019-09-28 14:21:33 +0800

    安装axios,配置全局使用

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Description: 全局的入口
  * @Author: your name
  * @Date: 2019-09-25 22:40:24
- * @LastEditTime: 2019-09-26 17:10:11
+ * @LastEditTime: 2019-09-28 14:20:10
  * @LastEditors: Please set LastEditors
  */
 import Vue from 'vue'
@@ -13,6 +13,11 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // 引入初始化样式
 import '../src/styles/index.less'
+// 配置axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// 将axios给vue的原型
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
